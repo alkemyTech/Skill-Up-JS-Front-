706fca5f3c52e537c74d6f4fbf8003e1328c9730
Mldamico <37380030+Mldamico@users.noreply.github.com>
2022-11-07 18:33:24 -0300

    Added navbar

---
+++ src/utils/NavLink.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link, NavLink as BaseNavLink } from 'react-router-dom'
+
+export const NavLink = React.forwardRef(
+  ({ activeClassName, activeStyle, ...props }, ref) => {
+    return (
+      <BaseNavLink
+        ref={ref}
+        {...props}
+        className={({ isActive }) =>
+          [
+            props.className,
+            'py-3 pl-6',
+            isActive ? activeClassName : null,
+          ]
+            .filter(Boolean)
+            .join(" ")
+        }
+        style={({ isActive }) => ({
+          ...props.style,
+          ...(isActive ? activeStyle : null),
+        })}
+      />
+    );
+  }
+);
