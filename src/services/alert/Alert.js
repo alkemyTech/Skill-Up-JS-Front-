0335a29d1f2c5b6e9e42dfd7d350f63f5a5346cf
ckachuk <62022941+ckachuk@users.noreply.github.com>
2022-11-08 15:38:49 -0300

    Setup alert (#5)
    * build: the sweetalert2 dependency has been installed
    
    * feat: the alert service has been created
    
    * feat: the confirmation method has been implemented
    
    * feat: the error method has been implemented and Toast variable has become global
    
    * feat: the information method has been implemented
    
    * feat: the question method has been implemented
    
    * chore: ifs were replaced by ternary operations

---
+++ src/services/alert/Alert.js
@@ -0,0 +1,28 @@
+import Swal from 'sweetalert2'
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+export const alert = {
+  confirmation: (toast, title, text) => {
+    return toast === true ? Toast.fire({ icon: 'success', title, text }) : Swal.fire({ icon: 'success', title, text })
+  },
+  error: (toast, title, text) => {
+    return toast === true ? Toast.fire({ icon: 'error', title, text }) : Swal.fire({ icon: 'error', title, text })
+  },
+  information: (title, text) => {
+    return Swal.fire({ icon: 'info', title, text })
+  },
+  question: (title, text, textConfirmButton) => {
+    return Swal.fire({ icon: 'question', title, text, confirmButtonText: textConfirmButton, showCancelButton: true })
+  }
+}
