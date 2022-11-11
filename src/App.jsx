0cebd6371876715283d733ec435eb3387ac3fb7f
Juan Sevillano <sevillanojuanmanuel@gmail.com>
2022-11-11 17:05:53 -0300

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './Components/Navs/Navbar'
-<<<<<<< HEAD
-import { AdminPanel } from './pages/Admin/AdminPanel'
-=======
 import ProtectRoutes from './helpers/ProtectRoutes'
 import RedirectHome from './helpers/RedirectHome'
->>>>>>> main
 import { Balance } from './pages/Balance/Balance'
 import { Error404 } from './pages/Error404'
 import { Home } from './pages/Home'
@@ -18,39 +13,53 @@ import { Send } from './pages/Send/Send'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <Routes>
-          <Route element={<Home />}>
-            <Route path='/' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Route>
-          <Route element={<Navbar />}>
-            <Route path='/Balance' element={<Balance />} />
-            <Route path='/Movements' element={<Movements />} />
-            <Route path='/send' element={<Send />} />
-            <Route path='/admin' element={<AdminPanel />} />
-          </Route>
-
-          <Route path='*' element={<Error404 />} />
-        </Routes>
-      </CssBaseline>
-    </ThemeProvider>
-=======
     <Routes>
       <Route element={<Home />}>
-        <Route path='/Login' element={<RedirectHome><Login /></RedirectHome>} />
-        <Route path='/register' element={<RedirectHome><Register /></RedirectHome>} />
+        <Route
+          path='/Login'
+          element={
+            <RedirectHome>
+              <Login />
+            </RedirectHome>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <RedirectHome>
+              <Register />
+            </RedirectHome>
+          }
+        />
       </Route>
       <Route element={<Navbar />}>
-        <Route path='/' element={<ProtectRoutes><Balance /></ProtectRoutes>} />
-        <Route path='/Movements' element={<ProtectRoutes><Movements /></ProtectRoutes>} />
-        <Route path='/send' element={<ProtectRoutes><Send /></ProtectRoutes>} />
+        <Route
+          path='/'
+          element={
+            <ProtectRoutes>
+              <Balance />
+            </ProtectRoutes>
+          }
+        />
+        <Route
+          path='/Movements'
+          element={
+            <ProtectRoutes>
+              <Movements />
+            </ProtectRoutes>
+          }
+        />
+        <Route
+          path='/send'
+          element={
+            <ProtectRoutes>
+              <Send />
+            </ProtectRoutes>
+          }
+        />
       </Route>
       <Route path='*' element={<Error404 />} />
     </Routes>
->>>>>>> main
   )
 }
 
